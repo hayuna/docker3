8de93edc4c7de9e980940e48e321178617252385
mras2303 <marek.kowalonek@gmail.com>
2019-11-10 21:00:37 +0000

    add client

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const OtherPage = () => (
+    <div>
+        Im some other page!
+        <Link to='/'>Go back home</Link>
+    </div>
+)
+
+export default OtherPage
